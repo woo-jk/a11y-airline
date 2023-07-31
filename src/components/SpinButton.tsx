c4f44c2ca9c5c0119b054bf6f73383de5d60f4e8
woo-jk <201702042@o.cnu.ac.kr>
2023-07-31 15:50:11 +0900

    feat: 웹 접근성 추가

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -3,13 +3,16 @@ import "./SpinButton.css";
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
+
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
 
   const increment = () => {
+    if (count >= 3) return;
     setCount((prevCount) => prevCount + 1);
   };
 
   const decrement = () => {
+    if (count <= 0) return;
     setCount((prevCount) => prevCount - 1);
   };
 
@@ -23,28 +26,24 @@ const SpinButton: React.FC = () => {
         <h1>승객 선택</h1>
         <div className="spinButtonLabel">
           <label>성인</label>
-          <div
-            className="helpIcon"
-            onMouseEnter={toggleTooltip}
-            onMouseLeave={toggleTooltip}
-          >
-            ?
-            {isTooltipVisible && (
-              <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
-            )}
+          <div className="helpIcon" onMouseEnter={toggleTooltip} onMouseLeave={toggleTooltip}>
+            ?{isTooltipVisible && <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button onClick={decrement} className="spinButton" aria-label="성인 탑승자 한명 줄이기">
           -
         </button>
+
         <input
           type="text"
           role="spinbutton"
           readOnly
           className="spinButtonInput"
           value={count}
+          aria-label={`성인 승객 ${count}`}
+          aria-live="assertive"
         />
-        <button onClick={increment} className="spinButton">
+        <button onClick={increment} className="spinButton" aria-label="성인 탑승자 한명 늘리기">
           +
         </button>
       </div>
